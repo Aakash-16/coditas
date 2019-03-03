3d7192c3491482f79d4bcc31f01b8ccece2fdb7d
Aakash-16 <aakashdd35@gmail.com>
2019-03-03 15:11:50 +0530

    Wordcount Program completed

--- WordCount/src/org/coditas/assignment/Map.java
+++ WordCount/src/org/coditas/assignment/Map.java
@@ -1,19 +1,26 @@
 package org.coditas.assignment;
 
+import java.io.IOException;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class Map extends Mapper<LongWritable, Text, Text, IntWritable>{
-	
+
 	@Override
-	public void map(LongWritable offset , Text line,Context context){
-		
+	public void map(LongWritable offset , Text line,Context context) throws IOException, InterruptedException{
+		String lineStr = line.toString();
+		String tokens[] = lineStr.split(" ");
+		for (int count = 0 ; count <tokens.length;count++ ) {
+			line.set(tokens[count]);
+			context.write(line, new IntWritable(1));
+		}
 	}
-		
-	
-	
-	
+
+
+
+
 
 }
--- WordCount/src/org/coditas/assignment/WordCount.java
+++ WordCount/src/org/coditas/assignment/WordCount.java
@@ -1,10 +1,30 @@
 package org.coditas.assignment;
 
+import java.io.IOException;
+
+import javax.xml.soap.Text;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
 public class WordCount {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+	Configuration configuration = new Configuration();
+	Job job =  Job.getInstance(configuration, "WordCount");
+	job.setJarByClass(WordCount.class);
+	job.setMapperClass(Map.class);
+	job.setReducerClass(Reduce.class);
+	job.setOutputKeyClass(Text.class);
+	job.setOutputValueClass(IntWritable.class);
+	FileInputFormat.setInputPaths(job, new Path(args[0]));
+	FileOutputFormat.setOutputPath(job, new Path(args[1]));
+			
 	}
 
 }
